21971bd19c21a6d6a368c6cbbafd200f826a9566
Kaushik603 <kaushikp@Technoidentity.com>
2026-02-09 12:39:11 +0530

    Add hooks-based data fetching playground

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import DashboardShell from "./features/Dashboard/DashboardShell";
+import DataFetchingContainer from "./features/Playground/DataFetchingContainer";
 
 const App = () => {
-  return <DashboardShell />;
+  return <DataFetchingContainer />;
 };
 
 export default App;
